d56fe9dcd91930d3365ab3bfb2d8f46aa5cfeafa
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2023-12-15 17:38:11 -0600

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,21 @@
 import './Palette.css'
+import { useState } from 'react'
+
 
 const Profile = () => {
+  const[color, setColor] = useState('')
   return (
-    <div className='flex flex-col border-2 p-8 items-center text-white'>
+    <div className='flex flex-col border-l-2 p-8 items-center text-white'>
+      <div className="relative text-gray-600">
+        <input type="text" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" onChange={ (event) => setColor(event.target.value) }/>
+
+      </div>
+      <div className='h-10 w-10/12 rounded' style={{ backgroundColor: color }}></div>
+      
 
         
 
-        <div className="palette">
+        <div className="palette mt-6">
           <div style={{ backgroundColor: '#ff2299'}}></div>
           <div style={{ backgroundColor: '#ee2299'}}></div>
           <div style={{ backgroundColor: '#dd2299'}}></div>
@@ -21,9 +30,7 @@ const Profile = () => {
         </div>
 
         <h1>@kodom</h1>
-        {/* <div className="flex w-1/2">
-            <img src={kaylee} alt="" className='rounded-full'/>
-        </div> */}
+
 
     </div>
   )
