b21a450cf4fc6fbf1a274bc9d0af3dd518a9d142
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2023-12-15 17:37:38 -0600

    Update PostHue.tsx

--- src/components/PostHue.tsx
+++ src/components/PostHue.tsx
@@ -10,14 +10,15 @@ const PostHue = (props:Props) => {
     const[color, setColor] = useState('')
 
   return (
-    <div className='flex flex-row p-4 justify-evenly gap-8'>
+    <div className='flex flex-col p-4 justify-evenly gap-8 p-5 rounded mt-8'>
 
-        <div className='flex flex-col w-full p-4 gap-4' style={{backgroundColor: color}}>
-            <input type="text" name="hue" id="hue" onChange={ (event) => setColor(event.target.value) } />
-            <a href="#" onClick={ () => props.addHue(color)  } className="btn bg-white text-cyan-950 text-center">Post</a>
+        <div className='flex flex-col w-full p-4 gap-4 rounded' style={{backgroundColor: 'grey'}}>
+            <input type="text" placeholder="Hexcode" name="hue" id="hue" className = "bg-white h-10 px-5 pr-10 rounded text-sm focus:outline-none" onChange={ (event) => setColor(event.target.value) } />
+            <a href="#" onClick={ () => props.addHue(color)  } className="btn bg-white text-cyan-950 text-center rounded w-">Post!</a>
         </div>
 
         <Hue hue={ {color, username:"kaylee", likes:3, isLiked: false}}  />
+
 
     </div>
   )
