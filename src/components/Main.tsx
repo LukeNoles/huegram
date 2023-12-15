343c152715eee1a6af18b4ab697a878b31eb2030
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2023-12-15 17:36:43 -0600

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,19 +12,23 @@ interface Props {
 
 const Main = ({hues, addHue, toggleLike} : Props) => {
   return (
-    <div className='flex flex-wrap w-full justify-center gap-8 overflow-y-auto'>
-
+    <div className='grid grid-cols-4'>
+      <div className='bg-gradient-to-r from-red-600 via-green-500 to-purple-600 inline-block text-transparent bg-clip-text text-6xl mt-10 ml-10'>
+        <h1>Huegram</h1>
+      </div>
+      <div className="col-start-1 col-end-2">
         <PostHue addHue={addHue}/>
-
-
+      </div>
+      <div className='mt-10 col-start-2 col-end-5 flex flex-wrap w-full justify-center gap-8 overflow-y-auto' id='parentID'>        
         {hues.map( (hue) => ( 
-            
             <Hue hue={hue} toggleLike={toggleLike}/>
        ))}
-
-        
+      </div>
 
     </div>
+
+
+    
   )
 }
 
